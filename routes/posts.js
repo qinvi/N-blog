b8f3b1ab157bec9caf8aac8a6b471a0040cddaa3
qinvi <qinvi.zeng@gmail.com>
2019-03-13 12:40:41 +0800

    添加文章更新跟删除，更新Post聚集方法

--- routes/posts.js
+++ routes/posts.js
@@ -27,8 +27,9 @@ router.post('/create', checkLogin, function (req, res, next) {
   const content = req.fields.content
 
   const params = { author, title, content }
+  const errorMes = { title: '请填写标题', content: '请填写内容' }
   // 校验参数
-  verifyParams(params, req, res)
+  verifyParams(params, errorMes, req, res)
 
   // 数据库写入
   writeDataToDB(params, req, res, next)
@@ -40,7 +41,7 @@ router.post('/create', checkLogin, function (req, res, next) {
  * @param {Object数据库写入} req
  * @param {Object} res
  */
-function writeDataToDB(data, req, res) {
+function writeDataToDB(data, req, res, next) {
   PostModel.create(data)
     .then(function (result) {
       // 此 post 是插入 mongodb 后的值，包含 _id
@@ -50,27 +51,6 @@ function writeDataToDB(data, req, res) {
       res.redirect(`/posts/${data._id}`)
     })
     .catch(next)
-}
-
-/**
- * 参数校验
- * @param {Object} params
- * @param {Object} req
- * @param {Object} res
- */
-function verifyParams(params, req, res) {
-  const { title, content } = params
-  try {
-    if (!title.length) {
-      throw new Error('请填写标题')
-    }
-    if (!content.length) {
-      throw new Error('请填写内容')
-    }
-  } catch (e) {
-    req.flash('error', e.message)
-    return res.redirect('back')
-  }
 }
 
 // GET /posts/create 发表文章页
@@ -101,17 +81,68 @@ router.get('/:postId', function (req, res, next) {
 
 // GET /posts/:postId/edit 更新文章页
 router.get('/:postId/edit', checkLogin, function (req, res, next) {
-  res.send('更新文章页')
+  const postId = req.params.postId
+
+  PostModel.getRawPostById(postId)
+    .then(function(post) {
+      if (!post) {
+        throw new Error('文章不存在')
+      }
+      res.render('edit', {
+        post: post
+      })
+    })
+    .catch(next)
 })
 
 // POST /posts/:postId/edit 更新一篇文章
 router.post('/:postId/edit', checkLogin, function (req, res, next) {
-  res.send('更新文章')
+  // author title content
+  const title = req.fields.title
+  const content = req.fields.content
+  const postId = req.params.postId
+
+  const params = { content, title }
+  const errorMes = { title: '请填写标题', title: '请填写内容' }
+  // 参数校验
+  verifyParams(params, errorMes, req, res)
+
+  // 数据库写入
+  PostModel.updatePostById(postId, params)
+    .then(function() {
+      req.flash('success', '编辑文章成功')
+      res.redirect(`/posts/${postId}`) // 跳转到文章新的详情页
+    })
+    .catch(next)
 })
 
 // GET /posts/:postId/remove 删除一篇文章
 router.get('/:postId/remove', checkLogin, function (req, res, next) {
-  res.send('删除文章')
+  const postId = req.params.postId
+  PostModel.delPostById(postId)
+  .then(function() {
+    req.flash('success', '删除文章成功')
+    res.redirect('/posts')
+  })
+  .catch(next)
 })
+
+/**
+ * 参数校验、目前只校验length
+ * @param {Object} params
+ * @param {Object} errorMes 错误报告信息
+ * @param {Object} req
+ * @param {Object} res
+ */
+function verifyParams(params, errorMes, req, res) {
+  try {
+    Object.keys(params).forEach(key => {
+      if (!params[key].length) throw new Error(errorMes[key])
+    })
+  } catch (e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+}
 
 module.exports = router
